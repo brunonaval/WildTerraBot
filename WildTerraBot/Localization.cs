b7c6b85c851648db0571be2d66f2178f21eb26b3
brunonaval <testadortiger@gmail.com>
2026-04-30 23:59:11 -0300

    Adiciona infraestrutura inicial de localização da DLL

---
+++ WildTerraBot/Localization.cs
@@ -0,0 +1,30 @@
+﻿using System.Globalization;
+using System.Threading;
+
+namespace WildTerraBot
+{
+    internal static class Localization
+    {
+        public static void SetCulture(string cultureName)
+        {
+            if (string.IsNullOrWhiteSpace(cultureName))
+                return;
+
+            CultureInfo culture;
+            try
+            {
+                culture = CultureInfo.GetCultureInfo(cultureName);
+            }
+            catch (CultureNotFoundException)
+            {
+                return;
+            }
+
+            Thread.CurrentThread.CurrentCulture = culture;
+            Thread.CurrentThread.CurrentUICulture = culture;
+            CultureInfo.DefaultThreadCurrentCulture = culture;
+            CultureInfo.DefaultThreadCurrentUICulture = culture;
+            Properties.Resources.Culture = culture;
+        }
+    }
+}
